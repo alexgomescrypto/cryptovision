169b22a2970168fc2bbfcb54247fc753eb2c2926
alexgomescrypto <alexgomescrypto@gmail.com>
2025-05-10 04:45:21 -0300

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://tnpysphwjohiaqpyedwv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRucHlzcGh3am9oaWFxcHllZHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NDI5ODEsImV4cCI6MjA2MjMxODk4MX0.u2j14xiFUUdedIB6HOJNQ6fPRdsCoFmnEGCSDYgSmuE';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
